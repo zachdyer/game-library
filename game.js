34dd7c49780c31d6b2fc41e810cb41de30b086b5
Zach Dyer <zach.dyer@zachdyerdesign.com>
2015-06-01 05:57:36 -0500

    Added a speed per second function

--- game.js
+++ game.js
@@ -4,7 +4,8 @@ function Game(canvasID) {
 	document.body.style.padding = 0;
 	document.body.style.overflow = "hidden";
 	document.body.style.cursor = "none";
-	
+
+	//Allows you to set the cursor type or turn it off
 	this.cursor = function(bool, type) {
 		if(bool) {
 			document.body.style.cursor = type;
@@ -12,45 +13,41 @@ function Game(canvasID) {
 			document.body.style.cursor = "none";
 		}
 	}
-	
-	//Can't use the this keyword in functions unless I save it in a variable and I have no idea why
+
+	//Importing the Game object for function use
 	var self = this;
-	
+
 	this.canvasElement = document.getElementById(canvasID);
-	
-	this.screen = {};
-	this.screen.width = window.innerWidth;
-	this.screen.height = window.innerHeight;
-	
-	//Canvas full screen with black background
-	this.canvasElement.width = this.screen.width;
-	this.canvasElement.height = this.screen.height;
-	this.canvasElement.style.backgroundColor = "black";
-	
-	//Canvas size updates when window size changes
-	window.onresize = function() {
+
+	this.screen = new Object;
+	this.screen.adjust=function (){
 		self.screen.width = window.innerWidth;
 		self.screen.height = window.innerHeight;
 		self.canvasElement.width = self.screen.width;
 		self.canvasElement.height = self.screen.height;
-	};
-	
+	}
+
+	this.canvasElement.style.backgroundColor = "black";
+
+	//Canvas size updates when window size changes
+	window.onresize = function(){self.screen.adjust()};
+	this.screen.adjust();
+
 	this.context = this.canvasElement.getContext("2d");
-	
-	var interval;
-	
+
 	var clearScreen = function(){
 		self.context.clearRect(0, 0, self.screen.width, self.screen.height);
 	};
-	
+
 	//Export FPS
 	this.fps = 0;
-	
-	//FPS Stuff
+
+	//FPS Private stuff
 	var fps = {};
 	fps.currentTime = new Number();
 	fps.lastTime = 0;
-	fps.updateTime = Date.now();
+	fps.timePerTick = 17;
+	fps.updateTime = Date.now(); //This sets a time stamp every second to update the Game.fps
 	fps.get = function(currentTime, lastTime) {
 		var fps = 1000 / (currentTime - lastTime);
 		return fps.toFixed();
@@ -61,19 +58,23 @@ function Game(canvasID) {
 			self.fps = fps.get(fps.currentTime, fps.lastTime);
 			fps.updateTime = fps.currentTime;
 		}
+		fps.timePerTick = fps.currentTime - fps.lastTime;
 		fps.lastTime = fps.currentTime;
+
 	};
-	
-	var step = function(custom){
-		fps.update();
-		clearScreen();
-		custom();
+
+	//Other function
+	this.speedPerSecond = function(speed) {
+		return speed / fps.timePerTick;
 	};
-	
-	this.loop = function (custom) {
-	interval = setInterval(function(){
-		step(custom);
-		},16);
-	};
-	
+
+	//This starts the main game loop
+	this.loop = function (custom){
+		requestAnimationFrame(function(){
+			fps.update();
+			clearScreen();
+			custom();
+			self.loop(custom);
+		})
+ 	};
 }
